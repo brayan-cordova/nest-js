6ce51c184c6b11a0908cace747164f651aaa47b2
bcordovag <lcordova@roo.com.gt>
2024-03-31 15:23:43 -0600

    creating service

--- 01-names/src/modules/names/names.controller.ts
+++ 01-names/src/modules/names/names.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { NamesService } from './names.service';
 
 @Controller('api/v1/names')
-export class NamesController {}
+export class NamesController {
+  constructor(private namesService: NamesService) {}
+}
