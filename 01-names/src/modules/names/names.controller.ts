a124106da9217348670c94207f0d04380dce2beb
bcordovag <lcordova@roo.com.gt>
2024-03-31 15:40:56 -0600

    operation Create with decorators @Post and @Body

--- 01-names/src/modules/names/names.controller.ts
+++ 01-names/src/modules/names/names.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { NamesService } from './names.service';
 
 @Controller('api/v1/names')
 export class NamesController {
   constructor(private namesService: NamesService) {}
+
+  @Post()
+  createName(@Body() data: { name: string }) {
+    return this.namesService.createName(data.name);
+  }
 }
--- 01-names/src/modules/names/names.service.ts
+++ 01-names/src/modules/names/names.service.ts
@@ -7,4 +7,10 @@ export class NamesService {
   constructor() {
     this._names = [];
   }
+
+  createName(name: string) {
+    this._names.push(name);
+    console.log('Names: ', this._names);
+    return true;
+  }
 }
