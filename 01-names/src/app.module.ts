1ba9508a0ca9b17432779946320e45d0d7b1e754
bcordovag <lcordova@roo.com.gt>
2024-03-31 15:12:18 -0600

    creating module

--- 01-names/src/app.module.ts
+++ 01-names/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NamesModule } from './modules/names/names.module';
 
 @Module({
-  imports: [],
+  imports: [NamesModule],
   controllers: [AppController],
   providers: [AppService],
 })
